888644ea27e2bbdc3372f28674c1d7e416f7b3f5
eymeryp <eymeryp@pc-dg-039-10>
2017-02-17 14:28:59 +0100

    Merge origin/master
    Conflicts:
    	src/Lecteur.java

--- src/Lecteur.java
+++ src/Lecteur.java
@@ -142,18 +142,14 @@ public class Lecteur implements Serializable
                 }
                 
                 public void lierEmprunt(Emprunt emp) {
-                    this.empruntsEnCours
+                    this.empruntsEnCours.add(emp);
                 }
                 
                 public int verifNombreEmprunts(){
                     return empruntsEnCours.size();
                 }
                 
-                public void ajouterExemplaire(Ouvrage o, Lecteur l, Exemplaire e){
-                    e.ajouterEmprunt(o, l, e);
-                    
-                    
-                }
+                
        
 		
 	
