b2b3b9b1e6f3de0ef15aed8d6d2543d13c9aa882
gueneler <gueneler@pc-dg-039-07>
2017-02-28 14:27:56 +0100

    histo lecteurs

--- src/Bibliotheque.java
+++ src/Bibliotheque.java
@@ -108,6 +108,7 @@ public class Bibliotheque implements Serializable
 		
 	}
 
+        // Création d'un ouvrage
 	public void nouvelOuvrage()
 	{
 		String numISBN = EntreesSorties.lireChaine("Entrez le numero ISBN de l'ouvrage : ");
@@ -142,6 +143,7 @@ public class Bibliotheque implements Serializable
 
 	}
 
+        // Création d'un exemplaire pour un ouvrage donné
 	public void nouvelExemplaire()
 	{
 		String numISBN = EntreesSorties.lireChaine("Entrez le numero ISBN de l'ouvrage : ");
@@ -211,6 +213,7 @@ public class Bibliotheque implements Serializable
 		}
 	}
         
+        // Affiche toutes les infos d'un ouvrage donné (numISBN, titre, nom de l'auteur, de l'éditeur, date de parution, le public cible et le nombre d'exemplaires)
         public void consulterOuvrage(){
                 String numOuvrage = EntreesSorties.lireChaine("Entrez le numéro ISBN de l'ouvrage à consulter : ");
                 
@@ -223,6 +226,7 @@ public class Bibliotheque implements Serializable
                 }
         }
         
+        //Affiche le numéro ISBN, le titre, et les infos de chaque exemplaires d'un ouvrrage donné
         public void consulterExemplaire(){
             String numOuvrage = EntreesSorties.lireChaine("Entrez le numéro ISBN de l'ouvrage à consulter : ");
             
@@ -237,6 +241,7 @@ public class Bibliotheque implements Serializable
             }
         }
         
+        //Affiche les emprunts en cours pour un lecteur donné
         public void consulterEmpruntsLecteur(){
             int numLecteur = EntreesSorties.lireEntier("Entrez le numéro de lecteur dont vous voulez vérifier les emprunts en cours : ");
             Lecteur l = unLecteur(numLecteur);
@@ -249,12 +254,13 @@ public class Bibliotheque implements Serializable
         }
 
 
-
+        // Affiche les emprunts dont non retournés 15 jours après leur date de retour prévue
         public void relancerLecteur() {
             int compteur = afficherRetardataires();
             System.out.println("Fin d'affichage des "+compteur+" emprunts en retard.");
         }
         
+        // Un lecteur emprunte un exemplaire d'un ouvrage
         public void emprunterExemplaire(){
             String numISBN = EntreesSorties.lireChaine("Entrez un numéro ISBN : ");
             int numExemplaire = EntreesSorties.lireEntier("Entrez le numéro d'exemplaire à emprunter : ");
@@ -323,6 +329,7 @@ public class Bibliotheque implements Serializable
             
         }
         
+        // Un lecteur retourne un exemplaire emprunté 
         public void rendreExemplaire(){
             String numISBN = EntreesSorties.lireChaine("Entrez un numéro ISBN : ");
             int numExemplaire = EntreesSorties.lireEntier("Entrez le numéro d'exemplaire rendu : ");
@@ -362,6 +369,7 @@ public class Bibliotheque implements Serializable
             }
         }
         
+        // Affiche l'historique des emprunts pour un lecteur donné
         public void histoLecteurs(){
             int numLecteur;
             Lecteur l; 
@@ -381,6 +389,7 @@ public class Bibliotheque implements Serializable
             EntreesSorties.afficherMessage("Fin d'affichage des "+compteur+" emprunts de ce lecteur.\n");
         }
         
+        // Affiche le nombre de fois qu'un ouvrage donné a été emprunté (tout exemplaire confondu)
         public void histoOuvrage(){
             String numISBN;
             Ouvrage o;
@@ -397,6 +406,7 @@ public class Bibliotheque implements Serializable
             EntreesSorties.afficherMessage("Cet ouvrage a été emprunté "+compteur+" fois !\n");
         }
         
+        // Affiche l'ouvrage le plus emprunté, et le nombre de fois où celui-ci a été emprunté
         public void topOuvrage(){
             Ouvrage top=null;
             int topActuel = 0;
@@ -451,6 +461,7 @@ public class Bibliotheque implements Serializable
 		return _dicoLecteur.get(numLecteur);
 	}
         
+        // Renvoi l'ouvrage correspondant au numISBN donné.
 	private Ouvrage getOuvrage(String numISBN){
 		return _dicoOuvrage.get(numISBN);
 	}
@@ -463,7 +474,8 @@ public class Bibliotheque implements Serializable
 	{
 		_dicoLecteur.put(numLecteur, L);
 	}
-
+        
+        // Ajoute l'ouvrage à la base de donnée de bibliotheque
 	private void lierOuvrage(Ouvrage O, String numISBN) { _dicoOuvrage.put(numISBN, O); }
 
 	
@@ -482,7 +494,8 @@ public class Bibliotheque implements Serializable
         
        
         /*
-         * Propose le premier numéro de lecteur disponible
+         * Propose le premier numéro de lecteur disponible. Ne pas passer par la taille du dictionnaire de lecteur permettra
+         * de prendre en compte les éventuelles suppressions de lecteur. 
          */        
         private int PropNumLecteur(){
             int numProp = 0;
@@ -495,6 +508,7 @@ public class Bibliotheque implements Serializable
             return numProp;
         }
 
+        //Utilisée par relancerLecteurs(). Renvoi le nombre d'emprunts en cours pour lesquels le lecteur est en retard
         private int afficherRetardataires() {
             int compteur = 0;
             GregorianCalendar dateActuelle;
@@ -512,10 +526,12 @@ public class Bibliotheque implements Serializable
             return compteur;
         }
         
+        // Ajoute l'emprunt au dictionnaire d'emprunts.
         public void ajouterEmpruntDico(Emprunt emp){
             _historiqueEmprunts.add(emp);
         }
         
+        // Retourne un emprunt présent dans le dictionnaire d'emprunts, à partir d'un numéro ISBN et d'un numéro d'exemplaire
         private Emprunt getEmprunt(String numISBN, int numExemplaire){
             Emprunt emp = null;
             for (Emprunt e : _historiqueEmprunts){
